979a4ee39dedf1eb25f666a5dc3b44cbd9b078ab
洪瑋成 <weicheng2138@gmail.com>
2016-04-22 18:45:41 +0800

    complete form builder of angular 2

--- dev/knights/new-knight.component.ts
+++ dev/knights/new-knight.component.ts
@@ -2,52 +2,44 @@ import {Component, OnInit} from "angular2/core";
 import {KnightService} from "./knight.service";
 import {Knight} from "./knight";
 import {Router, RouteParams} from "angular2/router";
+import {ControlGroup, FormBuilder, Validators, Validator} from "angular2/common";
 
 @Component({
     template: `
         <h2>Create a Knight here...</h2>
-        <form #myForm="ngForm" (ngSubmit)="onSubmit()">
+        <form [ngFormModel]="myForm" (ngSubmit)="onSubmit(myForm.value)">
             <div>
                 <label>Name: </label>
-                <input type="text" id="name"
-                    ngControl="name"
-                    [(ngModel)]="newKnight.name"
-                    required
+                <input type="text" id="name" 
+                [ngFormControl]="myForm.controls['name']" 
                 >
             </div>
             <div>
                 <label>Level: </label>
                 <input type="number" id="level"
-                    ngControl="level"
-                    [(ngModel)]="newKnight.level"
-                    required
+                [ngFormControl]="myForm.controls['level']"
                 >
+                <span *ngIf="!myForm.controls['level'].valid">Not Valid</span>
             </div>
             <div>
                 <label>Armor: </label>
                 <input type="text" id="armor"
-                    ngControl="armor"
-                    [(ngModel)]="newKnight.armor"
-                    required
+                [ngFormControl]="myForm.controls['armor']"
                 >
             </div>
             <div>
                 <label>Sword: </label>
                 <input type="text" id="sword"
-                    ngControl="sword"
-                    [(ngModel)]="newKnight.sword"
-                    required
+                [ngFormControl]="myForm.controls['sword']"
                 >
             </div>
             <div>
                 <label>Health: </label>
                 <input type="number" id="health"
-                    ngControl="health"
-                    [(ngModel)]="newKnight.health"
-                    required
+                [ngFormControl]="myForm.controls['health']"
                 >
             </div>
-            <button type="submit" [disabled]="!myForm.form.valid">Create Knight</button>
+            <button type="submit" [disabled]="!myForm.valid">Create Knight</button>
         </form>
         `,
     styles: [`
@@ -69,7 +61,7 @@ import {Router, RouteParams} from "angular2/router";
             padding: 0px;
         }
         .ng-invalid{
-            border: 2px solid red;
+            border: 1px solid red;
         }
     `],
     providers: [KnightService]
@@ -78,19 +70,27 @@ import {Router, RouteParams} from "angular2/router";
 })
 export class NewKnightComponent implements OnInit{
 
-    public newKnight: Knight;
+    myForm: ControlGroup;
 
-    constructor(private _knightService: KnightService, private _router: Router, private _routeParams: RouteParams) {}
+    constructor(
+        private _knightService: KnightService,
+        private _router: Router,
+        private _routeParams: RouteParams,
+        private _formBuilder: FormBuilder
+    ) {}
 
     ngOnInit():any {
-        this.newKnight = {name: this._routeParams.get('name'), level: null, armor: '', sword: '', health: null};
-        // this.passedArmor = this._routeParams.get('armor');
-        // this.knight = <Knight>this._routeParams.params;
+        this.myForm = this._formBuilder.group({
+            'name': [this._routeParams.get('name'), Validators.required],
+            'level': ['', Validators.required],
+            'armor': ['', Validators.required],
+            'sword': ['', Validators.required],
+            'health': ['', Validators.required]
+        });
     }
 
-
-    onSubmit() {
-        this._knightService.insertKnight(this.newKnight);
+    onSubmit(value) {
+        this._knightService.insertKnight(value);
         this._router.navigate(['Knights']);
     }
 
